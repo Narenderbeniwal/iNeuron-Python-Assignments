7c1086529f6181a0833298eac9d66724b28d7b1f
Narender Kumar <narenderbeniwal1234@gmail.com>
2021-12-27 08:01:53 +0530

    Add files via upload

---
+++ Python Advance Assignment 15.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+	•	What are the new features added in Python 3.8 version?
+	•	What is monkey patching in Python?
+	•	What is the difference between a shallow copy and deep copy?
+	•	What is the maximum possible length of an identifier?
+	•	What is generator comprehension?
+
+# ## Q.1 What are the new features added in Python 3.8 version?
+# 
+# Sol :
+#     Python 3.8 adds some new syntax to the language, a few minor changes to existing behavior, and mostly a bunch of speed improvements — maintaining the tradition from the earlier 3.7 release.
+
+# ## Q. 2 What is monkey patching in Python?
+# 
+# Sol: In Python, the term monkey patch refers to dynamic (or run-time) modifications of a class or module. In Python, we can actually change the behavior of code at run-time.
+
+# ## Q.3 What is the difference between a shallow copy and deep copy?
+# 
+# A shallow copy constructs a new compound object and then (to the extent possible) inserts references into it to the objects found in the original. 
+# 
+# A deep copy constructs a new compound object and then, recursively, inserts copies into it of the objects found in the original.
+# 
+# ![Difference-Between-Shallow-Copy-and-Deep-Copy-Comparison-Summary.jpeg](attachment:Difference-Between-Shallow-Copy-and-Deep-Copy-Comparison-Summary.jpeg)
+
+# ## Q.4 What is the maximum possible length of an identifier?
+# 
+# In Python, the highest possible length of an identifier is 79 characters
+
+# ## Q.5 What is generator comprehension?
+# 
+# Sol: A generator comprehension is a single-line specification for defining a generator in Python. It is absolutely essential to learn this syntax in order to write simple and readable code. Note: Generator comprehensions are not the only method for defining generators in Python.
+
+# In[ ]:
+
+
+
+
